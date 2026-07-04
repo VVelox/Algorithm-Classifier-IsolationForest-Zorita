ccbb816d98c4b728de61861d85b8245b61f1eb33
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-07-04 14:49:09 -0500

    initial files

---
+++ lib/Algorithm/Classifier/IsolationForest/Zorita.pm
@@ -0,0 +1,105 @@
+package Algorithm::Classifier::IsolationForest::Zorita;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Algorithm::Classifier::IsolationForest::Zorita - The great new Algorithm::Classifier::IsolationForest::Zorita!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Algorithm::Classifier::IsolationForest::Zorita;
+
+    my $foo = Algorithm::Classifier::IsolationForest::Zorita->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-algorithm-classifier-isolationforest-zorita at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Algorithm-Classifier-IsolationForest-Zorita>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Algorithm::Classifier::IsolationForest::Zorita
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Algorithm-Classifier-IsolationForest-Zorita>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Algorithm-Classifier-IsolationForest-Zorita>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Algorithm-Classifier-IsolationForest-Zorita>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2026 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
+
+
+=cut
+
+1; # End of Algorithm::Classifier::IsolationForest::Zorita
